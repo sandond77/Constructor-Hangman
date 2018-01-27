1d68bb72f8d258f133d8bf5c39fc65519bc1200b
sandond77 <sandu@ucdavis.edu>
2018-01-26 23:55:25 -0800

    complete

--- index.js
+++ index.js
@@ -3,23 +3,15 @@ var Word = require('./word.js')
 var guesses = 10;
 var seed = Math.floor((Math.random() * 5));
 var flag = false;
-var dogs = ["husky", "corgi", "beagle","shiba", "boxer"];
-// var mammals = ["gorilla", "elephant", "dolphin", "monkey", "rabbit", "manatee", "squirrel", "hedgehog","otter","raccoon"];
-// var animals = ["chameleon", "penguin", "jellyfish", "brachiosaurus", "kangaroo", "chinchilla", "walrus", "human","catfish","triceratops"];
+var dogs = ["husky", "corgi", "beagle", "poodle", "shiba", "bulldog","terrier", "pug", "boxer", "chihuahua"];
 var booleanArray = [];
 var winCheck = false;
 var currentWord = new Word(dogs[seed]);
+var guessedLetters = [];
 
 function game(){
-	// inquirer.prompt([
-	// 	{
-	// 		type:"list",
-	// 		name:"Choose a Category",
-	// 		choices: ["Dogs(easy)","Mammals(intermediate)","Animals(Hard)"]
-
-	// 	}
-	// ])
 	winCheck = false;
+	guesses = 10;
 	currentWord.addArray();
 	currentWord.underscorer();
 	prompter();
@@ -35,6 +27,8 @@ function prompter(){
 	]).then(function(response){
 		response.guess = response.guess.toLowerCase();
 
+		guessedLetters.push(response.guess);
+
 		for (var i = 0; i < dogs[seed].length; i++) {
 			currentWord.word[i].repeat(response.guess)
 			booleanArray.push(currentWord.word[i].changed)
@@ -48,19 +42,33 @@ function prompter(){
 		} else if (results===false) {
 			console.log("\nCorrect! \n");		
 		}
+
 		booleanArray = [];
 		currentWord.underscorer();
 	}).then(function(){
 		for (var i = 0; i < dogs[seed].length; i++) {
 			currentWord.word[i].changed = false
 		}
-		
+
+		var winCheckArray = currentWord.showLetter;
+		winCheckArray = winCheckArray.replace(/\s/g,''); 
+
+		if (winCheckArray === dogs[seed]) {
+			winCheck = true;
+		}
+
 		if (winCheck === true) {
 			console.log("You Win!") 
-		} else if (guesses > 0 && currentWord.showLetter === false){
+			console.log("---------------------------------!")
+			seed = Math.floor((Math.random() * 5));
+			currentWord = new Word(dogs[seed]);
+
+			replay();
+		} else if (guesses > 0 && winCheck===false){
 			prompter();
-		} else if (guesses === 0){
-			console.log("You Lose!")
+		} else if (guesses === 0 && winCheck===false){
+			console.log("You Lose!\n")
+			replay();
 		}
 	})
 }
@@ -69,7 +77,19 @@ function isFalse(arg) {
   return arg === false;
 }
 
+function replay(){
+	inquirer.prompt([
+		{
+		type:"confirm",
+		name: "replay",
+		message: "Would You Like to Play Again?: ",
+		default: true
+		}
+	]).then(function(response){	
+		if (response.replay) {
+			game();
+		}
+	});
+}
 
 game();
-
-//Need to fix win conditional; lose and looped guessing is working properly.
